6a93f4fda753b230b1bf0c03e3ded2753266b49b
João <mutantinotrue@outlook.com>
2023-05-16 21:29:12 -0300

    Fixing setters

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,31 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
-        Filme filme1 = new Filme();
-        filme1.setNome("Velozes e Furiosos");
-        filme1.setAnoDeLancamento(1978);
-        filme1.setDuracao(188);
-        filme1.setAvalia(10);
-        filme1.setAvalia(10);
-        filme1.setAvalia(10);
-        filme1.setincluidoNoPlano(false);
-        System.out.println(filme1.getNome());
 
-        //System.out.println(filme1);
+        // Using Class Filme
+        Filme velozesefuriosos = new Filme();
+        velozesefuriosos.setNome("Velozes e Furiosos");
+        velozesefuriosos.setAnoDeLancamento(1978);
+        velozesefuriosos.setDuracao(188);
+        velozesefuriosos.setAvalia(10);
+        velozesefuriosos.setAvalia(10);
+        velozesefuriosos.setAvalia(10);
+        velozesefuriosos.setincluidoNoPlano(false);
+        velozesefuriosos.exibeFichaTecnica();
 
+        //Using Class Serie
+        Serie peacemaker = new Serie();
+        peacemaker.setNome("PeaceMaker");
+        peacemaker.setMinutosPorEpisodio(320);
+        peacemaker.setEpisodiosPorTemporada(8);
+        peacemaker.setTemporada(1);
+        peacemaker.setAnoDeLancamento(2022);
+        peacemaker.setAvalia(10);
+        peacemaker.setAvalia(8);
+        peacemaker.setAtiva(false);
+        peacemaker.exibeFichaTecnica();
 
     }
 }
